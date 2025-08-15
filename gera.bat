d12a4f615349b3a935ddc337463bb46169b59c8d
Lawrence Teixeira <lawrence@lawrence.eti.br>
2025-08-15 14:23:24 -0300

    modified:   gera.bat

--- gera.bat
+++ gera.bat
@@ -1,2 +1,2 @@
-docker build --no-cache -t lawrenceteixeira/autodoc_v20 -f Dockerfile_AutoDoc .
-docker push lawrenceteixeira/autodoc_v20
+docker build --no-cache -t lawrenceteixeira/autodoc_v21 -f Dockerfile_AutoDoc .
+docker push lawrenceteixeira/autodoc_v21
